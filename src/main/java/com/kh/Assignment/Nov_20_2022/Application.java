29b184bcc7d7157d34df7dc7cce33dcd5eea6974
Hisabori <101242920+Hisabori@users.noreply.github.com>
2022-11-21 11:25:59 +0900

    commit #3 / Study 22.11.21 @ Han Jeonghun

--- src/main/java/com/kh/Assignment/Nov_20_2022/Application.java
+++ src/main/java/com/kh/Assignment/Nov_20_2022/Application.java
@@ -1,22 +1,24 @@
 package com.kh.Assignment.Nov_20_2022;
 
 import com.kh.Assignment.Nov_20_2022.Code.Array;
-import com.kh.Assignment.Nov_20_2022.Code.Control;
-import com.kh.Assignment.Nov_20_2022.Code.Member;
 
 public class Application {
+
+    private static Object method4;
 
     public static void main(String[] args) {
         //System.out.println("test");
 
-        new Control().method1();
-        new Control().method2();
+        //new Control().method1();
+        //new Control().method2();
 
-        new Array().method1();
-        new Array().method2();
-        new Array().method3();
+        //new Array().method1();
+        //new Array().method2();
+        //new Array().method3();
+        //new Array().method4();
+        new Array().method5(method4);
 
-        new Member().method1();
+       // new Member().method1();
 
     }
 }
