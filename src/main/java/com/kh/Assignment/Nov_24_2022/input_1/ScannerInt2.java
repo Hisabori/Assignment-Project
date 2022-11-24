6d7d33cf4344946b3fecb7ebe75b30ed9bed164c
Hisabori <101242920+Hisabori@users.noreply.github.com>
2022-11-25 01:12:30 +0900

    25 Nov 2022 HAN Jeonghun Commit #2
    codeUp 사이트 문제 풀이
    1010-1014

---
+++ src/main/java/com/kh/Assignment/Nov_24_2022/input_1/ScannerInt2.java
@@ -0,0 +1,20 @@
+package com.kh.Assignment.Nov_24_2022.input_1;
+
+import java.util.Scanner;
+
+public class ScannerInt2 {
+
+    public void testInt2(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("test");
+
+        int test = sc.nextInt();
+        int test2 = sc.nextInt();
+        System.out.println(test);
+        System.out.println(test2);
+
+
+
+    }
+
+}
---
+++ src/main/java/com/kh/Assignment/Nov_24_2022/input_1/ScannerTest2.java
@@ -0,0 +1,22 @@
+package com.kh.Assignment.Nov_24_2022.input_1;
+
+
+
+import  java.util.Scanner;
+public class ScannerTest2 {
+    public  void test(){
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("test");
+        String test=sc.nextLine();
+
+        System.out.println("test2");
+        String test2=sc.nextLine();
+
+
+        System.out.println(test2);
+        System.out.println(test);
+
+
+    }
+}
