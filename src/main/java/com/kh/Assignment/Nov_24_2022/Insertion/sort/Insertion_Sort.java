b111dc11865c8ca20843463fe17ceb3314a0a7a2
Hisabori <101242920+Hisabori@users.noreply.github.com>
2022-11-24 12:42:16 +0900

    24 Nov 2022 @ first commit by han jeonghun
    @과제 (Insertion_Sort)

---
+++ src/main/java/com/kh/Assignment/Nov_24_2022/Insertion/sort/Insertion_Sort.java
@@ -0,0 +1,32 @@
+package com.kh.Assignment.Nov_24_2022.Insertion.sort;
+
+import java.util.Arrays;
+
+public class Insertion_Sort {
+    static int [] nums;
+    public void method1(){
+           //static int[] nums;
+        nums = new int[10];
+        for (int i = 0; i <10; i++){
+            nums[i]= (int)(Math.random()*10);
+        }
+        System.out.println("before");
+        System.out.println(Arrays.toString(nums));
+
+        for (int i = 1; i<nums.length; i++){
+            int temp=nums[i];
+            int prev = i-1;
+            while(prev>=0 && nums[prev]>temp){
+                nums[prev+1]=nums[prev];
+                prev--;
+
+            }
+            nums[prev+1]=temp;
+        }
+        System.out.println("after");
+        System.out.println(Arrays.toString(nums));
+
+    }
+
+}
+
---
+++ src/main/java/com/kh/Assignment/Nov_24_2022/Insertion/sort/Main.java
@@ -0,0 +1,7 @@
+package com.kh.Assignment.Nov_24_2022.Insertion.sort;
+
+public class Main {
+    public static void main(String[] args) {
+        new Insertion_Sort().method1();
+    }
+}
