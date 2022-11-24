b523d2d2f4849f88c7fab55e4918026ae2c89214
Hisabori <101242920+Hisabori@users.noreply.github.com>
2022-11-25 01:24:03 +0900

    Revert "25 Nov 2022"
    This reverts commit 6d7d33cf4344946b3fecb7ebe75b30ed9bed164c.

--- src/main/java/com/kh/Assignment/Nov_24_2022/Main.java
+++ src/main/java/com/kh/Assignment/Nov_24_2022/Main.java
@@ -1,15 +1,13 @@
 package com.kh.Assignment.Nov_24_2022;
 
 import com.kh.Assignment.Nov_24_2022.Insertion.sort.Insertion_Sort;
-import com.kh.Assignment.Nov_24_2022.input_1.*;
+import com.kh.Assignment.Nov_24_2022.input_1.ScannerTest;
+import com.kh.Assignment.Nov_24_2022.input_1.ScannerInt;
 
 public class Main {
     public static void main(String[] args) {
         //new Insertion_Sort().method1();
         //new ScannerTest().test();
-        //new ScannerInt().testInt();
-        //new ScannerFloat().testFloat();
-        //new ScannerInt2().testInt2();
-        new ScannerTest2().test();
+        new ScannerInt().testInt();
     }
 }
