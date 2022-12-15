dff03742c8a02591188fd3f9aa5d5460ee0ac290
Hisabori <101242920+Hisabori@users.noreply.github.com>
2022-12-15 15:18:50 +0900

    22-12-15 배열 알고리즘
    This reverts commit 6d7d33cf

---
+++ src/main/java/Array_Sort/Array_Sort.java
@@ -0,0 +1,12 @@
+package Array_Sort;
+
+import java.util.Arrays;
+
+public class Array_Sort {
+    public void arraySort(){
+        int [] Array = new int[]{1,2,3,4,5};
+                Arrays.sort(Array);
+        System.out.println("Array="+Arrays.toString(Array));
+
+    }
+}
---
+++ src/main/java/Array_Sort/main.java
@@ -0,0 +1,7 @@
+package Array_Sort;
+
+public class main {
+    public static void main(String[] args) {
+        new Array_Sort1().StringArray1();
+        }
+    }
